4c9474851405dd3774366c190dc884cc7e3c00d0
Eli Perelman <eli@eliperelman.com>
2015-12-23 13:30:21 -0600

    Updating script

--- prey.sh
+++ prey.sh
@@ -7,6 +7,7 @@ TIME="14d"
 DEVICE="flame-kk"
 MEMORY="512"
 BRANCH="master"
+TEST="cold-launch"
 
 ./node_modules/.bin/raptor query measure \
   --host $DB_HOST \
@@ -17,19 +18,17 @@ BRANCH="master"
   --protocol $DB_PROTOCOL \
   --context $CONTEXT \
   --metric $METRIC \
+  --test $TEST \
   --time $TIME \
   --device $DEVICE \
   --memory $MEMORY \
-  --branch $BRANCH | \
-./node_modules/.bin/raptor regression | \
-./node_modules/.bin/raptor track \
+  --branch $BRANCH | ./node_modules/.bin/raptor regression | ./node_modules/.bin/raptor track \
   --host $DB_HOST \
   --port $DB_PORT \
   --username $DB_USERNAME \
   --password $DB_PASSWORD \
   --database $DB_NAME \
-  --protocol $DB_PROTOCOL | \
-./node_modules/.bin/raptor bug \
+  --protocol $DB_PROTOCOL | ./node_modules/.bin/raptor bug \
   --username $BZ_USERNAME \ 
   --password $BZ_PASSWORD \
   --token $BITLY_TOKEN
